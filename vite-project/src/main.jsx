cae1b1eaf5fdaab34a63ebb9cc8ffb8360ea64dd
desmond123-arch <desmondopokumends2004@gmail.com>
2024-11-06 22:46:13 +0000

    test without ssr

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import ReactDOM from 'react-dom';
 import './index.css'
 import App from './App.jsx'
 import { HelmetProvider } from 'react-helmet-async'
 
-createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'))
+root.render(
   <StrictMode>
     <HelmetProvider>
       <App />
     </HelmetProvider>
-  </StrictMode>,
+  </StrictMode>
 )
