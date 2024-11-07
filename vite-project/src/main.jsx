eee5a88aa36e05864094196970243593d046efe6
desmond123-arch <desmondopokumends2004@gmail.com>
2024-11-07 00:58:22 +0000

    test again

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,13 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { HelmetProvider } from 'react-helmet-async'
 
 const root = createRoot(document.getElementById('root'))
 root.render(
   <StrictMode>
-    <HelmetProvider>
       <App />
-    </HelmetProvider>
   </StrictMode>
 )
